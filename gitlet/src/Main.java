0268a93a46d1575ee047180f680700fa2c146518
Mustafa Tarek <mustafatarek412@gmail.com>
2024-08-29 22:24:33 +0200

    My Second Commit

--- gitlet/src/Main.java
+++ gitlet/src/Main.java
@@ -1,3 +1,4 @@
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -6,6 +7,6 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome from Mustafa");
 
-        
+    
     }
 }
