483cc7c4a413194fa8998f76b91b08bc09b0f147
Mustafa Tarek <mustafatarek412@gmail.com>
2024-09-05 05:41:31 +0200

    Command init implementation

---
+++ gitlet/src/gitlet/Head.java
@@ -0,0 +1,18 @@
+package gitlet;
+import java.io.File;
+public class Head {
+  private final File head_Dir;
+  public Head(File Head_Dir)
+  {
+  head_Dir=Head_Dir;
+  }
+   public void setHead(Branch branch) // set branch name at head file that head refers to
+   {
+          Utils.writeContents(head_Dir, branch.getName());
+   }
+     public String getHead()//get branch name that head refers to
+     {
+            return Utils.readContentsAsString(head_Dir);
+     }
+
+}
