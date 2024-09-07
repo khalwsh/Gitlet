0bbe17e31fbcd991940b06ec77c1a6916e1a61ae
Mustafa Tarek <mustafatarek412@gmail.com>
2024-09-07 18:44:05 +0200

    adjusting main

--- src/BranchStore.java
+++ src/BranchStore.java
@@ -7,7 +7,6 @@ public class BranchStore {
     public BranchStore(File Branches_Dir) {
         this.Branches_Dir = Branches_Dir;
     }
-<<<<<<< HEAD
        public void saveBranch(Branch branch)
        {
              File currentBranchFile=Utils.join(Branches_Dir, branch.getName());
@@ -52,40 +51,3 @@ public class BranchStore {
               saveBranch(newBranch);
        }
 }
-=======
-
-    public void saveBranch(Branch branch) {
-        File currentBranchFile = Utils.join(Branches_Dir, branch.getName());
-        Utils.writeObject(currentBranchFile, branch);
-    }
-
-    public Branch getBranch(String branchName) {
-        File branchFile = Utils.join(Branches_Dir, branchName);
-        if (!branchFile.exists()) return null;
-        return Utils.readObject(branchFile, Branch.class);
-    }
-
-    public ArrayList<Commit> getBranchHistory(Commit currCommit, CommitStore commitStore) {
-        ArrayList<Commit> listOfCommits = new ArrayList<Commit>();
-
-        listOfCommits.add(currCommit);
-        while (currCommit.getParentCommitHash() != null) {
-
-            String prevCommitHash = currCommit.getParentCommitHash();
-            Commit prevCommit = commitStore.getCommit(prevCommitHash);
-            listOfCommits.add(prevCommit);
-            currCommit = prevCommit;
-        }
-
-        return listOfCommits;
-    }
-
-    public boolean CheckBranchExistence(String branchName) {
-        File branchFile = Utils.join(Branches_Dir, branchName);
-        return branchFile.exists();
-    }
-    public String[]GetAllBranchesName() {
-        return Branches_Dir.list();
-    }
-}
->>>>>>> 963f9d64deea4867bac58f150fe2dc01e3cd2294
--- src/Main.java
+++ src/Main.java
@@ -10,47 +10,43 @@ public class Main {
 
         Repository repository = new Repository(System.getProperty("user.dir"));
 
-<<<<<<< HEAD
-        break;
-        
-        case "add":
-        
-        ValidateArgs(1, args.length-1);
-        repository.add(args[1]);
-        
-        break;
-        
-        case "commit":
-         ValidateArgs(1, args.length-1);
-         repository.commit(args[1]);
-         break;
-        
-        
-        
-        case "rm":
-         
-        ValidateArgs(1, args.length-1);
-        repository.rm(args[1]);
-        
-        break;
-        
-        
-        case "log":
-        ValidateArgs(0, args.length-1);
-         repository.log();
-=======
->>>>>>> 963f9d64deea4867bac58f150fe2dc01e3cd2294
 
-        switch (command) {
-            case "init":
+    switch (command) {
+        case "init":
                 ValidateArgs(0, args.length - 1);
 
                 repository.init();
 
-<<<<<<< HEAD
-         break;
+                  break;
+        case "add":
+             ValidateArgs(1, args.length-1);
+             repository.add(args[1]); 
+             break;
+
+        
+             case "commit":
+            ValidateArgs(1, args.length-1);
+            repository.commit(args[1]);
+            break;
+
          
-         case "checkout":
+            case "log":
+            ValidateArgs(0, args.length-1);
+            repository.log();
+            break;
+        
+            case "global-log":
+               ValidateArgs(0, args.length-1);
+               repository.globallog();
+               break;
+
+        
+            case "rm":
+               ValidateArgs(1, args.length-1);
+               repository.commit(args[1]);
+               break;
+        
+            case "checkout":
          
                if(args.length==3 &&args[1].equals("--"))
                {
@@ -84,72 +80,6 @@ public class Main {
             break;
        }
            
-=======
-                break;
-
-            case "add":
-
-                ValidateArgs(1, args.length - 1);
-                repository.add(args[1]);
-
-                break;
-
-            case "commit":
-                ValidateArgs(1, args.length - 1);
-                repository.commit(args[1]);
-                break;
-
-
-            case "rm":
-
-                ValidateArgs(1, args.length - 1);
-                repository.rm(args[1]);
-
-                break;
-
-
-            case "log":
-                ValidateArgs(0, args.length - 1);
-                repository.log();
-
-                break;
-
-            case "global-log":
-                ValidateArgs(0, args.length - 1);
-                repository.globallog();
-
-                break;
-
-            case "checkout":
-
-                if (args.length == 3 && args[1] == "--") {
-                    //java gitlet.Main checkout -- [file name]
-                    repository.CheckOutFile(args[2]);
-                    break;
-                } else if (args.length == 4 && args[2] == "--") {
-                    //java gitlet.Main checkout [commit id] -- [file name]
-                    repository.CheckOutFileByHash(args[1], args[3]);
-                    break;
-                } else if (args.length == 2) {
-                    //java gitlet.Main checkout [branch name]
-                    repository.CheckOutBranch(args[1]);
-                    break;
-                } else Utils.exitWithMessage("Incorrect Operands");
-
-                break;
-            case "find":
-                // it takes commit message and return the hashes of all commits has this message
-                ValidateArgs(1, args.length - 1);
-                repository.find(args[1]);
-                break;
-            case "status":
-                ValidateArgs(0, args.length - 1);
-                repository.status();
-            default:
-                break;
-        }
-
->>>>>>> 963f9d64deea4867bac58f150fe2dc01e3cd2294
     }
 
     private static void ValidateArgs(int n, int argsLength) {
