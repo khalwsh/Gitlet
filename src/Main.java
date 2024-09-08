54075080a001f807e7200d4b5c4af0c154003375
khalwsh <khaledlabeb552@gamil.com>
2024-09-08 03:27:49 +0000

    format adjusting , delete compliation files

--- src/Main.java
+++ src/Main.java
@@ -11,97 +11,93 @@ public class Main {
         Repository repository = new Repository(System.getProperty("user.dir"));
 
 
-    switch (command) {
-        case "init":
+        switch (command) {
+            case "init":
                 ValidateArgs(0, args.length - 1);
 
                 repository.init();
 
-                  break;
-        case "add":
-             ValidateArgs(1, args.length-1);
-             repository.add(args[1]); 
-             break;
+                break;
+            case "add":
+                ValidateArgs(1, args.length - 1);
+                repository.add(args[1]);
+                break;
 
-        
-             case "commit":
-            ValidateArgs(1, args.length-1);
-            repository.commit(args[1]);
-            break;
 
-         
+            case "commit":
+                ValidateArgs(1, args.length - 1);
+                repository.commit(args[1]);
+                break;
+
+
             case "log":
-            ValidateArgs(0, args.length-1);
-            repository.log();
-            break;
-        
+                ValidateArgs(0, args.length - 1);
+                repository.log();
+                break;
+
             case "global-log":
-               ValidateArgs(0, args.length-1);
-               repository.globallog();
-               break;
+                ValidateArgs(0, args.length - 1);
+                repository.globallog();
+                break;
 
             case "find":
-             ValidateArgs(1, args.length-1);
-             repository.find(args[1]);
-             break;
+                ValidateArgs(1, args.length - 1);
+                repository.find(args[1]);
+                break;
             case "rm":
-               ValidateArgs(1, args.length-1);
-               repository.rm(args[1]);
-               break;
+                ValidateArgs(1, args.length - 1);
+                repository.rm(args[1]);
+                break;
 
-         case "status":
-           ValidateArgs(0, args.length-1);
-           repository.status();
-           break;
+            case "status":
+                ValidateArgs(0, args.length - 1);
+                repository.status();
+                break;
 
             case "checkout":
-         
-               if(args.length==3 &&args[1].equals("--"))
-               {
-                        //java gitlet.Main checkout -- [file name]
-                        repository.CheckOutFile(args[2]);
-                           break;
-               }
-               else if(args.length==4 &&args[2].equals("--"))
-               {
-                         //java gitlet.Main checkout [commit id] -- [file name]
-                         repository.CheckOutFileByHash(args[1], args[3]);
-                         break;
-               }
-               else if(args.length==2)
-               {
-                   //java gitlet.Main checkout [branch name]
-                   repository.CheckOutBranch(args[1]);
-                   break;
-               }
-               else  {Utils.exitWithMessage("Incorrect Operands");}
-        case "branch":
-             ValidateArgs(1, args.length-1);
-              repository.branch(args[1]);
-              break;
 
-        case "rm-branch":
-               ValidateArgs(1, args.length-1);
-               repository.rmbranch(args[1]);
-               break;
-       case "add-remote":
-            
-              ValidateArgs(2, args.length-1);   
-               repository.addRemote(args[1],args[2]);
-               break;
-       case "rm-remote":
-       ValidateArgs(1, args.length-1);
-       repository.removeRemote(args[1]);
-       break;
-       
-         default:
-            break;
-       }
-           
+                if (args.length == 3 && args[1].equals("--")) {
+                    //java gitlet.Main checkout -- [file name]
+                    repository.CheckOutFile(args[2]);
+                    break;
+                } else if (args.length == 4 && args[2].equals("--")) {
+                    //java gitlet.Main checkout [commit id] -- [file name]
+                    repository.CheckOutFileByHash(args[1], args[3]);
+                    break;
+                } else if (args.length == 2) {
+                    //java gitlet.Main checkout [branch name]
+                    repository.CheckOutBranch(args[1]);
+                    break;
+                } else {
+                    Utils.exitWithMessage("Incorrect Operands");
+                }
+            case "branch":
+                ValidateArgs(1, args.length - 1);
+                repository.branch(args[1]);
+                break;
+
+            case "rm-branch":
+                ValidateArgs(1, args.length - 1);
+                repository.rmbranch(args[1]);
+                break;
+            case "add-remote":
+
+                ValidateArgs(2, args.length - 1);
+                repository.addRemote(args[1], args[2]);
+                break;
+            case "rm-remote":
+                ValidateArgs(1, args.length - 1);
+                repository.removeRemote(args[1]);
+                break;
+
+            default:
+                break;
+        }
+
     }
 
     private static void ValidateArgs(int n, int argsLength) {
         if (argsLength != n) Utils.exitWithMessage("Incorrect Operands");
     }
- 
+
 }
--- src/RemoteStore.java
+++ src/RemoteStore.java
@@ -2,31 +2,31 @@
 import java.io.File;
 public class RemoteStore {
     private final File Remote_Dir;
-    public RemoteStore(File Remote_Dir)
-    {
-         this.Remote_Dir=Remote_Dir;
+
+    public RemoteStore(File Remote_Dir) {
+        this.Remote_Dir = Remote_Dir;
     }
+
     //just create new file with remote name and path to it
-    public void addRemotePath(String remoteName,String remotePath)
-    {
-          File reomteFile=Utils.join(Remote_Dir, remoteName);
-          if(reomteFile.exists()) Utils.exitWithMessage("A remote with that name already exists.");
-            
-          
-         Utils.writeContents(reomteFile, remotePath);
-          File test=Utils.join(remotePath, "testfromlocal");
-          Utils.writeContents(test, "hello from local");
-        
+    public void addRemotePath(String remoteName, String remotePath) {
+        File reomteFile = Utils.join(Remote_Dir, remoteName);
+        if (reomteFile.exists()) Utils.exitWithMessage("A remote with that name already exists.");
+
+
+        Utils.writeContents(reomteFile, remotePath);
+        File test = Utils.join(remotePath, "testfromlocal");
+        Utils.writeContents(test, "hello from local");
+
     }
-    public void removeRemotePath(String remoteName)
-    {
-        File reomteFile=Utils.join(Remote_Dir, remoteName);
-        if(!reomteFile.exists()) Utils.exitWithMessage( "A remote with that name does not exist.");
+
+    public void removeRemotePath(String remoteName) {
+        File reomteFile = Utils.join(Remote_Dir, remoteName);
+        if (!reomteFile.exists()) Utils.exitWithMessage("A remote with that name does not exist.");
         reomteFile.delete();
     }
-    public String getRemotePath(String remoteName)
-    {
-        File reomteFile=Utils.join(Remote_Dir, remoteName);
+
+    public String getRemotePath(String remoteName) {
+        File reomteFile = Utils.join(Remote_Dir, remoteName);
         return Utils.readContentsAsString(reomteFile);
     }
 }
