7c78f487cba440cca14c5987154fdec5216382bd
Mustafa Tarek <mustafatarek412@gmail.com>
2024-09-09 10:53:25 +0200

    add push and fetch commands

--- src/BranchStore.java
+++ src/BranchStore.java
@@ -1,9 +1,17 @@
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.AbstractMap;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 public class BranchStore {
     private final File Branches_Dir;
-
+     
     public BranchStore(File Branches_Dir) {
         this.Branches_Dir = Branches_Dir;
     }
@@ -12,6 +20,7 @@ public class BranchStore {
              File currentBranchFile=Utils.join(Branches_Dir, branch.getName());
             Utils.writeObject(currentBranchFile, branch);
        }
+      
        public void deleteBranch(String branchName)
        {
              File branchFile=Utils.join(Branches_Dir, branchName);
@@ -24,25 +33,138 @@ public class BranchStore {
         return Utils.readObject(branchFile, Branch.class);
        }
 
-       public ArrayList<Commit> getBranchHistory(Commit currCommit, CommitStore commitStore)
+       public ArrayList<Commit> getBranchHistory(Commit curCommit, CommitStore commitStore)
        {
                 ArrayList<Commit>listOfCommits=new ArrayList<Commit>();
                 
-                    listOfCommits.add(currCommit);
-                  while (currCommit.getParentCommitHash()!=null) {
+                    listOfCommits.add(curCommit);
+                    
+                  while (curCommit.getParentCommitHash()!=null) {
 
-                    String prevCommitHash=currCommit.getParentCommitHash();
-                    Commit prevCommit=commitStore.getCommit(prevCommitHash);
+                   Commit prevCommit=getPreviousCommit(curCommit, commitStore);
+                   if(prevCommit==null) break;
                     listOfCommits.add(prevCommit);
-                    currCommit=prevCommit;
+                    curCommit=prevCommit;
                   }
                   
                 return listOfCommits;
        }
+       public ArrayList<Commit>getRemoteBranchHistory(Commit curCommit,String remoteName,RemoteStore remoteStore)
+       {
+
+        ArrayList<Commit>listOfCommits=new ArrayList<Commit>();
+                
+        listOfCommits.add(curCommit);
+        
+      while (curCommit.getParentCommitHash()!=null) {
+
+        String prevCommitHash=curCommit.getParentCommitHash();
+        Commit prevCommit=remoteStore.getRemoteCommit(prevCommitHash,remoteName);
+       if(prevCommit==null) break;
+        listOfCommits.add(prevCommit);
+        curCommit=prevCommit;
+      }
+      
+    return listOfCommits;
+      
+       }
+           Commit getPreviousCommit(Commit curCommit,CommitStore commitStore)
+           {
+            String prevCommitHash=curCommit.getParentCommitHash();
+                    Commit prevCommit=commitStore.getCommit(prevCommitHash);
+                    return prevCommit;
+           }
+           
+       public Map.Entry<Boolean, ArrayList<Commit>> checkIsAncestor(String remoteName,Branch remoteBranch,Commit curCommit,CommitStore commitStore,RemoteStore remoteStore)
+       {
+               String remoteHeadHash=remoteBranch.getReferredCommitHash();
+              
+
+                ArrayList<Commit>listOfCommits=new ArrayList<Commit>();
+               boolean Found=false;
+               while(true)
+               {
+                if(!remoteHeadHash.equals(curCommit.getCommitHash()))
+                {
+                  listOfCommits.add(curCommit);
+
+                  Commit prevCommit=getPreviousCommit(curCommit, commitStore);
+
+                  if(prevCommit==null) break;
+                  curCommit=prevCommit;
+                }
+                else {Found=true;break;}
+                      
+               }
+               //possibility for shared history
+               if(Found)
+               {
+                      Commit remoteCommit=remoteStore.getRemoteCommit(remoteHeadHash,remoteName);
+                      while (curCommit!=null && remoteCommit!=null ) {
+
+                        if(!remoteCommit.getCommitHash().equals(curCommit.getCommitHash())) 
+                        {
+                          //history is not identical
+                          Found=false;
+                          break;
+                        }
+                         Commit prevLocalCommit=getPreviousCommit(curCommit, commitStore);
+                         curCommit=prevLocalCommit;
+                         Commit prevRemoteCommit=getPreviousCommit(remoteCommit, commitStore);
+                         remoteCommit=prevRemoteCommit;
+                      }
+               }
+           
+               return new AbstractMap.SimpleEntry<>(Found,listOfCommits );
+             
+       }
+       public void CopyFromSrcToDist
+       (String firstSrc,String secondSrc,String firstDist,String secondDist,ArrayList<Commit>listOfCommits)
+       {
+        //copy commits and blobs
+             for(int i=0;i<listOfCommits.size();i++)
+             {
+
+              Commit curCommit=listOfCommits.get(i);
+              //copy commits
+              String hash=curCommit.getCommitHash();
+              Path commitSourcePath = Paths.get(firstSrc, hash);
+              Path commitDestPath = Paths.get(firstDist, hash);
+              copyFile(commitSourcePath, commitDestPath);
+              //copy blobs
+              Map<String,String>trackedFiles;
+              trackedFiles=curCommit.trackedFiles();
+
+              for (Map.Entry<String, String> entry : trackedFiles.entrySet()) {
+               String blob=entry.getValue();
+               Path blobSourcePath = Paths.get(secondSrc, blob);
+               Path blobDestPath = Paths.get(secondDist, blob);
+               copyFile(blobSourcePath, blobDestPath);
+            }
+
+             }
+        
+       }
+      
+       public static void copyFile(Path sourcePath, Path destinationPath) {
+        try {
+          // Ensure the parent directory exists
+          Files.createDirectories(destinationPath.getParent());
+
+          // Copy the file from source to destination
+          Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
+
+      } catch (IOException e) {
+          System.out.println("An error occurred while copying the file.");
+          e.printStackTrace();
+      }
+    }
+
        public boolean CheckBranchExistence(String branchName)
        {
                File branchFile=Utils.join(Branches_Dir, branchName);
                return branchFile.exists();
+               
        }
        public void createNewBranch(String branchName ,String commitHash)
        {
@@ -53,4 +175,5 @@ public class BranchStore {
        public String[]GetAllBranchesName() {
         return Branches_Dir.list();
     }
+
 }
--- src/RemoteStore.java
+++ src/RemoteStore.java
@@ -1,5 +1,6 @@
 
 import java.io.File;
+
 public class RemoteStore {
     private final File Remote_Dir;
     public RemoteStore(File Remote_Dir)
@@ -7,26 +8,81 @@ public class RemoteStore {
          this.Remote_Dir=Remote_Dir;
     }
     //just create new file with remote name and path to it
-    public void addRemotePath(String remoteName,String remotePath)
-    {
-          File reomteFile=Utils.join(Remote_Dir, remoteName);
-          if(reomteFile.exists()) Utils.exitWithMessage("A remote with that name already exists.");
-            
-          
-         Utils.writeContents(reomteFile, remotePath);
-          File test=Utils.join(remotePath, "testfromlocal");
-          Utils.writeContents(test, "hello from local");
-        
+   public void addRemotePath(String remoteName, String remotePath) {
+    // Create the directory for the remote
+    File remoteDir = Utils.join(Remote_Dir, remoteName);
+    
+    // Check if the remote directory already exists
+    if (remoteDir.exists()) {
+        Utils.exitWithMessage("A remote with that name already exists.");
     }
+
+    // Create the remote directory and subdirectories for blobs and commits
+    if (!remoteDir.mkdirs()) {
+        Utils.exitWithMessage("Failed to create the remote directory structure.");
+    }
+    
+    // Create the file to store the remote path
+    File remotePathFile = Utils.join(remoteDir, remoteName);
+        Utils.writeContents(remotePathFile, remotePath);
+
+}
+    
     public void removeRemotePath(String remoteName)
     {
-        File reomteFile=Utils.join(Remote_Dir, remoteName);
+        File reomteFile=Utils.join(Remote_Dir, remoteName,remoteName);
         if(!reomteFile.exists()) Utils.exitWithMessage( "A remote with that name does not exist.");
+
         reomteFile.delete();
     }
     public String getRemotePath(String remoteName)
     {
-        File reomteFile=Utils.join(Remote_Dir, remoteName);
+        File reomteFile=Utils.join(Remote_Dir,remoteName ,remoteName);
+        if(reomteFile.exists())
         return Utils.readContentsAsString(reomteFile);
+        return null;
+    }
+    public Branch getRemoteBranch(String remoteName,String remoteBranchName)
+    {
+       String remotePath=getRemotePath(remoteName);
+       if(remotePath ==null) Utils.exitWithMessage("Remote file is not exist");
+
+     File remotBranchFile=Utils.join(remotePath,"branches",remoteBranchName);
+     if(!remotBranchFile.exists()) return null;
+  
+        Branch remoteBranch=Utils.readObject(remotBranchFile, Branch.class);
+        return remoteBranch;
+          
+    }
+    public void saveRemoteBranchFile(String remoteName,Branch remoteBranch)
+    {
+        File remoteBranchFile=Utils.join(Remote_Dir, remoteName,remoteBranch.getName());
+          Utils.writeObject(remoteBranchFile,remoteBranch);
+    }
+    public void saveRemoteBranch(String remoteName,Branch remoteBranch)
+    {
+        String remotePath=getRemotePath(remoteName);
+        if(remotePath ==null) Utils.exitWithMessage("Remote file is not exist");
+ 
+      File remoteBranchFile=Utils.join(remotePath,"branches",remoteBranch.getName());
+        
+      Utils.writeObject(remoteBranchFile, remoteBranch);
+      
+    }
+    public Commit getRemoteCommit(String commitHash,String remoteName)
+    {
+     if(commitHash ==null) return null;
+
+     String remotePath=getRemotePath(remoteName);
+     if(remotePath ==null) Utils.exitWithMessage("Remote file is not exist");
+
+     File currentCommitFile=Utils.join(remotePath,"commits", commitHash);
+     
+    //////java.lang.IllegalArgumentException here
+         if(currentCommitFile.exists())
+         return Utils.readObject(currentCommitFile,Commit.class);
+         
+         return null;
+     
     }
 }
