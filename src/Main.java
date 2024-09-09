21c5e2ab612448a5dfdb0ce2d1f6fe70a56f0472
khalwsh <khaledlabeb552@gamil.com>
2024-09-09 13:08:16 +0000

    merge added in main

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         if (args.length == 0) {
             Utils.exitWithMessage("Please Enter command.");
         }
-        String command =args[0];
+        String command = args[0];
 
         Repository repository = new Repository(System.getProperty("user.dir"));
 
@@ -23,89 +23,87 @@ public class Main {
                 repository.add(args[1]);
                 break;
 
-        
-        case "commit":
-            ValidateArgs(1, args.length-1);
-            repository.commit(args[1]);
-            break;
 
-         
-        case "log":
-            ValidateArgs(0, args.length-1);
-            repository.log();
-            break;
-        
-        case "global-log":
-               ValidateArgs(0, args.length-1);
-               repository.globallog();
-               break;
+            case "commit":
+                ValidateArgs(1, args.length - 1);
+                repository.commit(args[1]);
+                break;
 
-        case "find":
-             ValidateArgs(1, args.length-1);
-             repository.find(args[1]);
-             break;
-        case "rm":
-               ValidateArgs(1, args.length-1);
-               repository.rm(args[1]);
-               break;
 
-        case "status":
-           ValidateArgs(0, args.length-1);
-           repository.status();
-           break;
+            case "log":
+                ValidateArgs(0, args.length - 1);
+                repository.log();
+                break;
 
-        case "checkout":
-         
-               if(args.length==3 &&args[1].equals("--"))
-               {
-                        //java gitlet.Main checkout -- [file name]
-                        repository.CheckOutFile(args[2]);
-                           break;
-               }
-               else if(args.length==4 &&args[2].equals("--"))
-               {
-                         //java gitlet.Main checkout [commit id] -- [file name]
-                         repository.CheckOutFileByHash(args[1], args[3]);
-                         break;
-               }
-               else if(args.length==2)
-               {
-                   //java gitlet.Main checkout [branch name]
-                   repository.CheckOutBranch(args[1]);
-                   break;
-               }
-               else  {Utils.exitWithMessage("Incorrect Operands");}
-        case "branch":
-             ValidateArgs(1, args.length-1);
-              repository.branch(args[1]);
-              break;
+            case "global-log":
+                ValidateArgs(0, args.length - 1);
+                repository.globallog();
+                break;
 
-        case "rm-branch":
-               ValidateArgs(1, args.length-1);
-               repository.rmbranch(args[1]);
-               break;
-        case "add-remote":
-            
-              ValidateArgs(2, args.length-1);   
-               repository.addRemote(args[1],args[2]);
-               break;
-       case "rm-remote":
-              ValidateArgs(1, args.length-1);
-              repository.removeRemote(args[1]);
-            break;
-        case "push":
-              ValidateArgs(2, args.length-1);
-            repository.push(args[1],args[2]);
-            break;
-       case "fetch":
-                 ValidateArgs(2, args.length-1);
-            repository.fetch(args[1],args[2]);
-            break;
+            case "find":
+                ValidateArgs(1, args.length - 1);
+                repository.find(args[1]);
+                break;
+            case "rm":
+                ValidateArgs(1, args.length - 1);
+                repository.rm(args[1]);
+                break;
 
-         default:
-            break;
-       }
-           
+            case "status":
+                ValidateArgs(0, args.length - 1);
+                repository.status();
+                break;
+
+            case "checkout":
+
+                if (args.length == 3 && args[1].equals("--")) {
+                    //java gitlet.Main checkout -- [file name]
+                    repository.CheckOutFile(args[2]);
+                    break;
+                } else if (args.length == 4 && args[2].equals("--")) {
+                    //java gitlet.Main checkout [commit id] -- [file name]
+                    repository.CheckOutFileByHash(args[1], args[3]);
+                    break;
+                } else if (args.length == 2) {
+                    //java gitlet.Main checkout [branch name]
+                    repository.CheckOutBranch(args[1]);
+                    break;
+                } else {
+                    Utils.exitWithMessage("Incorrect Operands");
+                }
+            case "branch":
+                ValidateArgs(1, args.length - 1);
+                repository.branch(args[1]);
+                break;
+
+            case "rm-branch":
+                ValidateArgs(1, args.length - 1);
+                repository.rmbranch(args[1]);
+                break;
+            case "add-remote":
+
+                ValidateArgs(2, args.length - 1);
+                repository.addRemote(args[1], args[2]);
+                break;
+            case "rm-remote":
+                ValidateArgs(1, args.length - 1);
+                repository.removeRemote(args[1]);
+                break;
+            case "push":
+                ValidateArgs(2, args.length - 1);
+                repository.push(args[1], args[2]);
+                break;
+            case "fetch":
+                ValidateArgs(2, args.length - 1);
+                repository.fetch(args[1], args[2]);
+                break;
+            case "merge":
+                ValidateArgs(1, args.length - 1);
+                repository.merge(args[1]);
+            default:
+                break;
+        }
+
     }
 
     private static void ValidateArgs(int n, int argsLength) {
